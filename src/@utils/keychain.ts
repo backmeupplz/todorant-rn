e4d30685a72b565e8dfbc13c270c26c1ce9f947d
backmeupplz <backmeupplz@gmail.com>
2020-03-20 19:57:55 -0700

    remove console log

--- src/@utils/keychain.ts
+++ src/@utils/keychain.ts
@@ -4,14 +4,12 @@ const KeychainManager = NativeModules.KeychainManager
 
 export function setToken(token: string) {
   if (Platform.OS === 'ios') {
-    console.log('setting token', token)
     KeychainManager.setToken(token)
   }
 }
 
 export function removeToken() {
   if (Platform.OS === 'ios') {
-    console.log('removing token')
     KeychainManager.removeToken()
   }
 }
