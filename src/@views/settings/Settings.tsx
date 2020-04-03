bf23cb75975e76d2977709ffc10ed9b7a5f4de37
backmeupplz <backmeupplz@gmail.com>
2020-04-02 17:44:48 -0700

    code push

--- src/@views/settings/Settings.tsx
+++ src/@views/settings/Settings.tsx
@@ -203,7 +203,7 @@ export class SettingsContent extends Component {
             </ListItem>
             <ListItem style={{ borderColor: sharedColors.placeholderColor }}>
               <Text style={{ color: sharedColors.placeholderColor }}>
-                v{DeviceInfo.getVersion()}.1
+                v{DeviceInfo.getVersion()}.2
                 {__DEV__ ? '.dev' : ''}
               </Text>
             </ListItem>
