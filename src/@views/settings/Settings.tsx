4e9a5a9e4f0966532b1bed39e9369fb353e600e4
backmeupplz <backmeupplz@gmail.com>
2020-04-25 06:57:52 -0700

    codepush

--- src/@views/settings/Settings.tsx
+++ src/@views/settings/Settings.tsx
@@ -37,7 +37,7 @@ import { alertSupport } from '@utils/alert'
 
 const Stack = createStackNavigator()
 
-const codePushVersion = 24
+const codePushVersion = 25
 
 @observer
 export class SettingsContent extends Component {
