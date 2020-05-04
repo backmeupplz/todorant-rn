b07e65274b4604d336d936f5e71a78946e421e23
backmeupplz <backmeupplz@gmail.com>
2020-05-04 16:17:00 -0700

    add activity indicator to pp and tos + add google calendar note

--- src/@views/settings/Rules.tsx
+++ src/@views/settings/Rules.tsx
@@ -6,7 +6,7 @@ import { observer } from 'mobx-react'
 
 @observer
 export class Rules extends Component {
-  rules = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(i =>
+  rules = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17].map((i) =>
     translate(`rules${i}`)
   )
 
--- src/@views/settings/TermsOfUse.tsx
+++ src/@views/settings/TermsOfUse.tsx
@@ -2,15 +2,27 @@ import React, { Component } from 'react'
 import { WebView } from 'react-native-webview'
 import { sharedColors } from '@utils/sharedColors'
 import { observer } from 'mobx-react'
+import { observable } from 'mobx'
+import { Spinner } from 'native-base'
 
 @observer
 export class TermsOfUse extends Component {
+  @observable loading = true
+
   render() {
     return (
-      <WebView
-        source={{ uri: 'https://todorant.com/terms' }}
-        style={{ flex: 1, backgroundColor: sharedColors.backgroundColor }}
-      />
+      <>
+        {this.loading && (
+          <Spinner style={{ backgroundColor: sharedColors.backgroundColor }} />
+        )}
+        <WebView
+          onLoadEnd={() => {
+            this.loading = false
+          }}
+          source={{ uri: 'https://todorant.com/terms' }}
+          style={{ flex: 1, backgroundColor: sharedColors.backgroundColor }}
+        />
+      </>
     )
   }
 }
