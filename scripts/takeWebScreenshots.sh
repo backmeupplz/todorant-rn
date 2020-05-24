253078be5be99debfcb15375b4ebefc0b9821e41
backmeupplz <backmeupplz@gmail.com>
2020-05-24 09:33:32 -0700

    add more screenshot steps

--- scripts/takeWebScreenshots.sh
+++ scripts/takeWebScreenshots.sh
@@ -1,2 +1,16 @@
 #!//bin/bash
 
+# Copy screenshot files
+cp scripts/screenshots/android/AppTest.java android/app/src/androidTest/java/com/todorant/AppTest.java
+cp scripts/screenshots/android/Fastfile android/fastlane/Fastfile
+
+# Launch Android emulator
+/Users/nikitakolmogorov/Library/Android/sdk/emulator/emulator -avd Pixel3API &
+
+# Create android screenshots
+cd android
+fastlane screenshots
+
+# Create ios screenshots
+cd ../ios
+fastlane snapshot
