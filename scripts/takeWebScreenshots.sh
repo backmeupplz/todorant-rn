656f7c5bada3e166b3fb2e69f932c6f013d606fe
backmeupplz <backmeupplz@gmail.com>
2020-05-24 09:45:53 -0700

    fix screenshot script

--- scripts/takeWebScreenshots.sh
+++ scripts/takeWebScreenshots.sh
@@ -4,8 +4,13 @@
 cp scripts/screenshots/android/AppTest.java android/app/src/androidTest/java/com/todorant/AppTest.java
 cp scripts/screenshots/android/Fastfile android/fastlane/Fastfile
 
+cp scripts/screenshots/ios/Snapfile ios/fastlane/Snapfile
+cp scripts/screenshots/ios/TodorantUITests.swift ios/TodorantUITests/TodorantUITests.swift
+
 # Launch Android emulator
 /Users/nikitakolmogorov/Library/Android/sdk/emulator/emulator -avd Pixel3API &
+# Start server
+yarn start &
 
 # Create android screenshots
 cd android
@@ -14,3 +19,4 @@ fastlane screenshots
 # Create ios screenshots
 cd ../ios
 fastlane snapshot
+
