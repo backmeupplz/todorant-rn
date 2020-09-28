df53e75d2007ad247aa26c8f62eae5eeeb7690db
purplenoodlesoop <68401994+purplenoodlesoop@users.noreply.github.com>
2020-09-28 19:00:43 +0300

    Fix formatting

--- ios/TodorantToday/SegmentedProgressBarView.swift
+++ ios/TodorantToday/SegmentedProgressBarView.swift
@@ -11,16 +11,22 @@ import SwiftUI
 struct SegmentedProgressBarView: View {
   var currentProgress: Int
   var maximumProgress: Int
-  
+
   var body: some View {
     HStack {
       Text(String(currentProgress))
         .font(.headline)
         .foregroundColor(.progressBar)
-        
-      maximumProgress < 15 ? AnyView(SegmentedProgressBarView.SegmentedProgressBar(currentProgress: currentProgress, maximumProgress: maximumProgress)) :
-        AnyView(SegmentedProgressBarView.LinearProgressBar(currentProgress: currentProgress, maximumProgress: maximumProgress))
-        
+
+      maximumProgress < 15 ? AnyView(SegmentedProgressBarView.SegmentedProgressBar(
+        currentProgress: currentProgress,
+        maximumProgress: maximumProgress
+      )) :
+        AnyView(SegmentedProgressBarView.LinearProgressBar(
+          currentProgress: currentProgress,
+          maximumProgress: maximumProgress
+        ))
+
       Text(String(maximumProgress))
         .font(.headline)
         .foregroundColor(.progressBar)
@@ -35,32 +41,34 @@ private extension SegmentedProgressBarView {
     var spacing: CGFloat = 3
     var selectedColor: Color = .progressBar
     var unselectedColor = Color.secondary.opacity(0.3)
-    
+
     var body: some View {
       HStack(spacing: spacing) {
         ForEach(0 ..< maximumProgress) { index in
           Capsule()
-            .foregroundColor(index < self.currentProgress ? self.selectedColor : self.unselectedColor)
+            .foregroundColor(index < self.currentProgress ? self.selectedColor : self
+              .unselectedColor)
         }
       }
       .frame(maxHeight: 2)
       .clipShape(Capsule())
     }
   }
-  
+
   struct LinearProgressBar: View {
     var currentProgress: Int
     var maximumProgress: Int
     var selectedColor: Color = .progressBar
     var unselectedColor = Color.secondary.opacity(0.3)
-    
+
     var body: some View {
       GeometryReader { geometry in
         ZStack(alignment: .leading) {
           Rectangle().frame(width: geometry.size.width)
             .foregroundColor(self.unselectedColor)
-          
-          Rectangle().frame(width: CGFloat(Int(geometry.size.width) / maximumProgress * currentProgress))
+
+          Rectangle()
+            .frame(width: CGFloat(Int(geometry.size.width) / maximumProgress * currentProgress))
             .foregroundColor(self.selectedColor)
         }
       }
