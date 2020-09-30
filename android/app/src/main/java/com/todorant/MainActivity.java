c8f76025edbb5e05575c5b9d4c966f0990a604a6
backmeupplz <backmeupplz@gmail.com>
2020-09-30 13:49:25 -0700

    add sharing to android

--- android/app/src/main/java/com/todorant/MainActivity.java
+++ android/app/src/main/java/com/todorant/MainActivity.java
@@ -7,6 +7,7 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 import org.devio.rn.splashscreen.SplashScreen;
+import android.content.Intent;
 
 public class MainActivity extends ReactActivity {
   @Override
@@ -32,5 +33,11 @@ public class MainActivity extends ReactActivity {
         return new RNGestureHandlerEnabledRootView(MainActivity.this);
       }
     };
+  }
+
+  @Override
+  public void onNewIntent(Intent intent) {
+    super.onNewIntent(intent);
+    setIntent(intent);
   }
 }
