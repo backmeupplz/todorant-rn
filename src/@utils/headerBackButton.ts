2800d6d248a2054ffe624f9ef0082447a271c467
backmeupplz <backmeupplz@gmail.com>
2020-10-06 09:48:45 -0700

    fix back buttons in settings

--- src/@utils/headerBackButton.ts
+++ src/@utils/headerBackButton.ts
@@ -1,8 +1,9 @@
 import { translate } from '@utils/i18n'
 
-export function headerBackButtonProps() {
+export function headerBackButtonProps(headerBackTitleVisible = true) {
   return {
     headerBackTitle: translate('back'),
     headerTruncatedBackTitle: translate('back'),
+    headerBackTitleVisible,
   }
 }
