24e1d1564311d43c3df97ace2c2aab39b47ccc92
Yakov Karpov <yakovkarpoff@gmail.com>
2020-10-28 19:30:12 +0300

    Create Target

---
+++ ios/Todorant-watchOS Extension/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Todorant-watchOS Extension
+//
+//  Created by Яков Карпов on 28.10.2020.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/Todorant-watchOS Extension/TodorantApp.swift
@@ -0,0 +1,22 @@
+//
+//  TodorantApp.swift
+//  Todorant-watchOS Extension
+//
+//  Created by Яков Карпов on 28.10.2020.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct TodorantApp: App {
+    @SceneBuilder var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+
+        WKNotificationScene(controller: NotificationController.self, category: "myCategory")
+    }
+}
