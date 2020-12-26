ed54c5cef5497dd814eb00957708e6dcdc25d3de
Yakov Karpov <yakovkarpoff@gmail.com>
2020-12-26 21:08:16 +0300

    Restore urls

--- src/@utils/sockets/socketIO.ts
+++ src/@utils/sockets/socketIO.ts
@@ -1,3 +1,5 @@
 import SocketIO from 'socket.io-client'
 
-export const socketIO = SocketIO('https://ws.todorant.com')
+export const socketIO = SocketIO(
+  __DEV__ ? 'http://localhost:3000' : 'https://ws.todorant.com'
+)
