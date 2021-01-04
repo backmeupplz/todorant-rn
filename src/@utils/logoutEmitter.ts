eab3fe6b122c5fa2537044a8842b1844719b8300
backmeupplz <backmeupplz@gmail.com>
2021-01-04 08:49:30 -0800

    remove logout emitter

--- src/@utils/logoutEmitter.ts
+++
@@ -1,9 +0,0 @@
-import { EventEmitter } from 'events'
-
-export const LogoutEvent = 'logout'
-
-export const logoutEventEmitter = new EventEmitter()
-
-export function requestLogout() {
-  logoutEventEmitter.emit(LogoutEvent)
-}
