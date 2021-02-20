78a69da243e5032c1f2a3762fe726abd7b06bfeb
backmeupplz <backmeupplz@gmail.com>
2021-02-20 09:06:23 -0800

    fix android sound

--- src/@utils/sound.ts
+++ src/@utils/sound.ts
@@ -1,30 +1,34 @@
 import { sharedSettingsStore } from '@stores/SettingsStore'
 import Sound from 'react-native-sound'
+import { Platform } from 'react-native'
 
 Sound.setMode('Default')
 Sound.setCategory('Ambient', true)
 
 const sounds = {} as { [index: string]: Sound }
 
-const level_up = new Sound(require('@assets/audio/level_up.mp3'), (error) => {
+function getSound(name: string, callback: (error: unknown) => void) {
+  return Platform.OS === 'android'
+    ? new Sound(`${name}.mp3`, Sound.MAIN_BUNDLE, callback)
+    : new Sound(require(`@assets/audio/${name}.mp3`), callback)
+}
+
+const level_up = getSound('level_up', (error) => {
   if (!error) {
     sounds.level_up = level_up
   }
 })
-const nice = new Sound(require('@assets/audio/nice.mp3'), (error) => {
+const nice = getSound('nice', (error) => {
   if (!error) {
     sounds.nice = nice
   }
 })
-const day_complete = new Sound(
-  require('@assets/audio/day_compele.mp3'),
-  (error) => {
-    if (!error) {
-      sounds.day_complete = day_complete
-    }
+const day_complete = getSound('day_compele', (error) => {
+  if (!error) {
+    sounds.day_complete = day_complete
   }
-)
-const task_done = new Sound(require('@assets/audio/task_done.mp3'), (error) => {
+})
+const task_done = getSound('task_done.mp3', (error) => {
   if (!error) {
     sounds.task_done = task_done
   }
