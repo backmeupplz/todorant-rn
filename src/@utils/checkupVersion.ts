79697821ae3bd643b1ad9f7cc1792f5bed16f674
Tasumaniadiabori <tasumaniadiabori@protonmail.com>
2021-03-12 21:06:59 +0300

    Add app gallery redirect to checkupVersion function

--- src/@utils/checkupVersion.ts
+++ src/@utils/checkupVersion.ts
@@ -1,10 +1,16 @@
 import { translate } from '@utils/i18n'
 import { Alert, Linking } from 'react-native'
 import { checkVersion } from 'react-native-check-version'
+import DeviceInfo from 'react-native-device-info'
 
 export async function checkupVersion() {
   try {
     const version = await checkVersion()
+    const installerPackageName = await DeviceInfo.getInstallerPackageName()
+    const url =
+      installerPackageName === 'com.huawei.appmarket'
+        ? 'https://appgallery.huawei.com/#/app/C102992095'
+        : version.url
 
     if (version.needsUpdate) {
       Alert.alert(
@@ -17,7 +23,7 @@ export async function checkupVersion() {
           },
           {
             text: translate('update'),
-            onPress: () => Linking.openURL(version.url),
+            onPress: () => Linking.openURL(url),
           },
         ],
         { cancelable: false }
