667595c94d9db71e15886763208fc6c4df9eeba7
ruslan zapevalov <upacyxou@gmail.com>
2022-03-31 22:42:52 +0400

    add prettier and autoimport settings to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,49 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'prettier',
+    'sort-imports-es6-autofix',
+    'import',
+    'no-relative-import-paths',
+    'eslint-plugin-node',
+  ],
   ignorePatterns: ['*.js'],
-  extends: ['plugin:prettier/recommended'],
+  extends: [
+    'plugin:prettier/recommended',
+    // 'eslint:recommended',
+    // 'plugin:@typescript-eslint/eslint-recommended',
+    // 'plugin:@typescript-eslint/recommended',
+    // '@react-native-community',
+  ],
   rules: {
     // '@typescript-eslint/no-explicit-any': 'error',
     // '@typescript-eslint/explicit-module-boundary-types': 'warn',
     'react-native/no-inline-styles': 'off',
+    'no-relative-import-paths/no-relative-import-paths': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        tabWidth: 2,
+        semi: false,
+        singleQuote: true,
+        endOfLine: 'auto',
+      },
+    ],
+    'sort-imports-es6-autofix/sort-imports-es6': [
+      2,
+      {
+        ignoreCase: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+      },
+    ],
   },
 }
