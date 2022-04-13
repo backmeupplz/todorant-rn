7a523bff6b904d2881c5edfa936431ce8214e11d
Tasumaniadiabori <tasumaniadiabori@protonmail.com>
2022-04-14 00:34:13 +0300

    Migrate TodoHeader from class to function componen

--- src/@components/TodoHeader.tsx
+++ src/@components/TodoHeader.tsx
@@ -1,99 +1,104 @@
 import 'moment/min/locales'
-import { Component } from 'react'
 import { IconButton } from '@components/IconButton'
+import { Observer } from 'mobx-react'
 import { Text, View } from 'native-base'
 import { TodoSectionType, sharedAppStateStore } from '@stores/AppStateStore'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import { capitalizeSentence } from '@utils/capitalizeSentence'
 import { checkSubscriptionAndNavigate } from '@utils/checkSubscriptionAndNavigate'
-import { observer } from 'mobx-react'
 import { sharedColors } from '@utils/sharedColors'
 import { sharedOnboardingStore } from '@stores/OnboardingStore'
 import { sharedSettingsStore } from '@stores/SettingsStore'
-import React from 'react'
+import React, { FC, memo, useCallback } from 'react'
 import fonts from '@utils/fonts'
 import moment from 'moment'
 
-@observer
-export class TodoHeader extends Component<{
+// TODO: figure out how callbacks affect on memo
+
+export const TodoHeader: FC<{
   item: string
   drag?: () => void
   isActive?: boolean
   date?: boolean
   onPlusPress?: () => void
   hidePlus?: boolean
-}> {
-  renderDateContent() {
-    return (
-      <>
-        {this.props.item}
-        {(this.props.item.length || 0) === 10 &&
-          `, ${capitalizeSentence(
-            moment(this.props.item)
-              .locale(sharedSettingsStore.language || 'en')
-              .format('dddd')
-          )}`}
-      </>
-    )
-  }
+}> = memo((props) => {
+  return (
+    <Observer>
+      {() => {
+        const renderDateContent = useCallback(() => {
+          return (
+            <>
+              {props.item}
+              {(props.item.length || 0) === 10 &&
+                `, ${capitalizeSentence(
+                  moment(props.item)
+                    .locale(sharedSettingsStore.language || 'en')
+                    .format('dddd')
+                )}`}
+            </>
+          )
+        }, [])
 
-  render() {
-    return (
-      <View
-        style={{
-          marginTop: 16,
-          marginBottom: 8,
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-        }}
-      >
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <TouchableOpacity
-            onLongPress={
-              sharedAppStateStore.todoSection === TodoSectionType.planning &&
-              sharedOnboardingStore.tutorialIsShown
-                ? this.props.drag
-                : undefined
-            }
+        return (
+          <View
+            style={{
+              marginTop: 16,
+              marginBottom: 8,
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
           >
-            <View
-              style={{
-                backgroundColor: 'rgba(255, 100, 26, 0.06)',
-                borderTopRightRadius: 6,
-                borderBottomRightRadius: 6,
-              }}
-            >
-              <Text
-                style={{
-                  color: sharedColors.primaryColor,
-                  marginVertical: 4,
-                  marginLeft: this.props.isActive ? 30 : 16,
-                  marginRight: 12,
-                  fontFamily: fonts.SFProRoundedRegular,
-                }}
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <TouchableOpacity
+                onLongPress={
+                  sharedAppStateStore.todoSection ===
+                    TodoSectionType.planning &&
+                  sharedOnboardingStore.tutorialIsShown
+                    ? props.drag
+                    : undefined
+                }
               >
-                {this.props.date ? this.renderDateContent() : this.props.item}
-              </Text>
+                <View
+                  style={{
+                    backgroundColor: 'rgba(255, 100, 26, 0.06)',
+                    borderTopRightRadius: 6,
+                    borderBottomRightRadius: 6,
+                  }}
+                >
+                  <Text
+                    style={{
+                      color: sharedColors.primaryColor,
+                      marginVertical: 4,
+                      marginLeft: props.isActive ? 30 : 16,
+                      marginRight: 12,
+                      fontFamily: fonts.SFProRoundedRegular,
+                    }}
+                  >
+                    {props.date ? renderDateContent() : props.item}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+              {!props.hidePlus && (
+                <IconButton
+                  disabled={!sharedOnboardingStore.tutorialIsShown}
+                  onPress={() => {
+                    props.onPlusPress
+                      ? props.onPlusPress()
+                      : checkSubscriptionAndNavigate('AddTodo', {
+                          date: props.item,
+                        })
+                  }}
+                  size={20}
+                  name="add_outline_28"
+                  color={sharedColors.primaryColor}
+                />
+              )}
             </View>
-          </TouchableOpacity>
-          {!this.props.hidePlus && (
-            <IconButton
-              disabled={!sharedOnboardingStore.tutorialIsShown}
-              onPress={() => {
-                this.props.onPlusPress
-                  ? this.props.onPlusPress()
-                  : checkSubscriptionAndNavigate('AddTodo', {
-                      date: this.props.item,
-                    })
-              }}
-              size={20}
-              name="add_outline_28"
-              color={sharedColors.primaryColor}
-            />
-          )}
-        </View>
-      </View>
-    )
-  }
-}
+          </View>
+        )
+      }}
+    </Observer>
+  )
+})
