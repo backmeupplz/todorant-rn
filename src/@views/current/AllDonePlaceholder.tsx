d1f57fd454597817d330a0c8d96918130bedefa3
Tasumaniadiabori <tasumaniadiabori@protonmail.com>
2022-04-20 01:12:31 +0300

    Migrate AllDonePlaceholder from class to function component

--- src/@views/current/AllDonePlaceholder.tsx
+++ src/@views/current/AllDonePlaceholder.tsx
@@ -1,51 +1,53 @@
-import { Component } from 'react'
 import { H1, Text, View } from 'native-base'
-import { observer } from 'mobx-react'
+import { Observer } from 'mobx-react'
 import { sharedColors } from '@utils/sharedColors'
 import { translate } from '@utils/i18n'
-import React from 'react'
+import React, { memo } from 'react'
 
-@observer
-export class AllDonePlaceholder extends Component {
-  render() {
-    return (
-      <View
-        style={{
-          flex: 1,
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <H1
-          style={{
-            color: sharedColors.textColor,
-            marginHorizontal: 24,
-            marginBottom: 12,
-          }}
-        >
-          🎉
-        </H1>
-        <H1
-          style={{
-            color: sharedColors.textColor,
-            opacity: 0.9,
-            marginHorizontal: 24,
-            marginBottom: 16,
-          }}
-        >
-          {translate('allDoneTitle')}
-        </H1>
-        <Text
-          style={{
-            textAlign: 'center',
-            color: sharedColors.textColor,
-            opacity: 0.8,
-            marginHorizontal: 24,
-          }}
-        >
-          {translate('allDoneText')}
-        </Text>
-      </View>
-    )
-  }
-}
+export const AllDonePlaceholder = memo(() => {
+  return (
+    <Observer>
+      {() => {
+        return (
+          <View
+            style={{
+              flex: 1,
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <H1
+              style={{
+                color: sharedColors.textColor,
+                marginHorizontal: 24,
+                marginBottom: 12,
+              }}
+            >
+              🎉
+            </H1>
+            <H1
+              style={{
+                color: sharedColors.textColor,
+                opacity: 0.9,
+                marginHorizontal: 24,
+                marginBottom: 16,
+              }}
+            >
+              {translate('allDoneTitle')}
+            </H1>
+            <Text
+              style={{
+                textAlign: 'center',
+                color: sharedColors.textColor,
+                opacity: 0.8,
+                marginHorizontal: 24,
+              }}
+            >
+              {translate('allDoneText')}
+            </Text>
+          </View>
+        )
+      }}
+    </Observer>
+  )
+})
